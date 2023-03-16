e44cc13815e568a1f2e61c852b4b42b648d45903
Jakub Olszewski <j.b.olszewski@gmail.com>
2023-03-16 22:41:16 +0100

    Biblioteka PDO 1/2

--- x055/index.php
+++ x055/index.php
@@ -14,6 +14,7 @@
                 Napisz skrypt, który :  
                 <ul>
                     <li>w pliku <code>index.php</code> nawiąże połączenie do bazy danych moje_praktyki</code></li>
+                    <li>połączy się za pomocą użytkownika (<code>praktykant</code>) i hasła(<code>zse</code>) do bazy danych moje_praktyki</code></li>
                     <li>wypisze dane z tabeli <code>harmonogram</code> w postaci tabeli HTML z użytymi stylami (ramka tabeli w kolorze #A74482)</li>
                     <li>w wypisanej tabeli będzie zawierał 7 wierszy w kolumnach <code>dzien (DATE), obowiazki (VARCHAR(128))</code></li>
                 </ul>  
@@ -21,10 +22,10 @@
                 <br>W pliku <code>kwerendy.sql</code> będą znajdować się wszystkie wykonane zapytania do bazy danych,
 
                 <br>Po wykonaniu ćwiczenia należy przesłać plik <code>imie.nazwisko.zip</code>,
-                <br><br>który będzie zawierał plik <code>index.php</code> oraz <code>kwerendy.sql</code>
-                <br><br>Uwaga. Należy wykorzystać bibliotekę PDO. 
-                <br><br>Wykonać odpowiednie zapytania tworzące bazę danych, tabelę, użytkownika, nadanie odpowiednich uprawnień 
-                <br><br>oraz wstawienie danych do tabeli w postaci 7 wierszy.
+                <br>który będzie zawierał plik <code>index.php, style.css oraz kwerendy.sql</code>
+                <br><br><b>Uwaga. Należy wykorzystać bibliotekę PDO. 
+                <br>Wykonać odpowiednie zapytania tworzące bazę danych, tabelę, użytkownika, nadanie odpowiednich uprawnień 
+                <br>oraz wstawienie danych do tabeli w postaci 7 wierszy.</b>
 
                 <!-- <br><br>Uwaga. Należy utworzyć funkcje <code>camel_case()</code> argumentem będzie ciąg znaków a w wyniku działania zwróci jedno słowo camelCase. -->
                 <!-- <a href="https://www.php.net/manual/en/function.str-ends-with.php">str_ends_with</a>. -->
